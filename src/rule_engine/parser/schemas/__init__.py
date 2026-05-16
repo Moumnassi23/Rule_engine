856bf9106bb09eb6846f959997e4ab0f2c42b2bc
Moumnassi23 <moumnassi245@gmail.com>
2026-05-16 16:44:40 +0200

    adding leafcondition and groupcondition

--- src/rule_engine/parser/schemas/__init__.py
+++ src/rule_engine/parser/schemas/__init__.py
@@ -1,6 +1,7 @@
 """Schemas Pydantic publics du parser."""
 from rule_engine.parser.schemas.inputs import InputDef
 from rule_engine.parser.schemas.metadata import RuleMetadata
+from rule_engine.parser.schemas.conditions import LeafCondition
 
 
-__all__ = ["InputDef", "RuleMetadata"]
+__all__ = ["InputDef", "RuleMetadata", "LeafCondition"]
